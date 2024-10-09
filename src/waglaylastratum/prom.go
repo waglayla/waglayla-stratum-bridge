8f732a787d5bbece90bb9bd0c1c1d12ff6fe9d64
TheKrueg1998 <80941977+TheKrueg1998@users.noreply.github.com>
2024-10-08 19:22:16 -0700

    Update prom.go

--- src/waglaylastratum/prom.go
+++ src/waglaylastratum/prom.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 
 	"github.com/waglayla/waglaylad/app/appmessage"
-	"github.com/waglayla/waglayla-stratum-bride/src/gostratum"
+	"github.com/waglayla/waglayla-stratum-bridge/src/gostratum"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
