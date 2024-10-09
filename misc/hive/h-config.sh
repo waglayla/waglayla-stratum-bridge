c2ffed327448cfc15691a0272e5b68b100b55a49
TheKrueg1998 <80941977+TheKrueg1998@users.noreply.github.com>
2024-10-08 17:49:24 -0700

    Naming Updates

--- misc/hive/h-config.sh
+++ misc/hive/h-config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-MINER_CONFIG="$MINER_DIR/$MINER_NAME/py_bridge.conf"
+MINER_CONFIG="$MINER_DIR/$MINER_NAME/wala_bridge.conf"
 mkfile_from_symlink $MINER_CONFIG
 
 
--- misc/hive/h-run.sh
+++ misc/hive/h-run.sh
@@ -2,4 +2,4 @@
 
 [[ ! -e ./config.yaml ]] && echo "missing config.yaml" && pwd && exit 1
 
-py_bridge  $(< py_bridge.conf)| tee --append $CUSTOM_LOG_BASENAME.log
+wala_bridge  $(< wala_bridge.conf)| tee --append $CUSTOM_LOG_BASENAME.log
