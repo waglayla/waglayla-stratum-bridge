f1e3f69883ccfaabb8462d84e556c4e1de12f4d2
TheKrueg1998 <80941977+TheKrueg1998@users.noreply.github.com>
2024-10-08 19:18:47 -0700

    Update hasher.go

--- src/waglaylastratum/hasher.go
+++ src/waglaylastratum/hasher.go
@@ -29,7 +29,7 @@ type waglaylaDiff struct {
 	targetValue *big.Int // previously know as fixedDifficultyBI
 }
 
-func newWaglaylaDiff() *waglaylaDiff {
+func newwaglaylaDiff() *waglaylaDiff {
 	return &waglaylaDiff{}
 }
 
