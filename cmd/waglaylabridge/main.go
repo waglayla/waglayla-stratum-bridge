e8186d820835a623efc081ec283b037e3a5737f5
Tritonn204 <tritonn204@gmail.com>
2024-11-06 14:02:15 -0800

    Corrected POW module dependency version

--- cmd/waglaylabridge/main.go
+++ cmd/waglaylabridge/main.go
@@ -58,8 +58,6 @@ func main() {
 		cfg.BlockWaitTime = 5 * time.Second // this should never happen due to waglayla 1s block times
 	}
 
-	cfg.SoloMining = true; // temporary for WALA only
-
 	log.Println("----------------------------------")
 	log.Printf("initializing bridge")
 	log.Printf("\twaglayla:          %s", cfg.RPCServer)
